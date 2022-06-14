505eeffe93f7f6b8955597b4be410cf611bbc1c8
maja <moram.imat.mail@gmail.com>
2022-06-14 23:27:46 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [show, setShow] = useState(false);
+
+  const array = [
+    { name: "Darko", surname: "Horvat", email: "ajmemeni@gmail.com", id: "1" },
+    { name: "Ana", surname: "Yeye", email: "anabanana@gmail.com", id: "2" },
+    { name: "Antonio", surname: "Banderas", email: "zoro@gmail.com", id: "3" },
+    {
+      name: "Jenny",
+      surname: "Block",
+      email: "jennyfromtheblock@gmail.com",
+      id: "4",
+    },
+    { name: "Jett", surname: "Viper", email: "jettviper@gmail.com", id: "5" },
+    { name: "Soho", surname: "Lou", email: "soholou@gmail.com", id: "6" },
+  ];
+
+  const list = array.map((item) => (
+    <>
+      <br />
+      <li>{item.name}</li>
+      <li>{item.surname}</li>
+      <li>{item.email}</li>
+    </>
+  ));
+
+  const howMany = array.length;
+
+  // const handleClick = () => {
+  //   howMany >+ 2 ? setShow(true) : setShow(list)
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Wow</h1>
+      <div>HOW MANY?</div>
+      <div>{howMany}</div>
+      <button onClick={() => setShow(true)}>click</button>
+      {(show && howMany >+ 2) ? (
+        <>
+          <div className="wrapper">
+            <ul className="lista">{list}</ul>
+          </div>
+        </>
+      ) : null}
+      {howMany === 3 ? <>{list}</> : howMany}
+      {howMany > 3 ? <>{list} + {howMany - 3}</> : howMany}
+    </>
   );
-}
+};
 
 export default App;
